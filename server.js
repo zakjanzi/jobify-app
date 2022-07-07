5e3be1695d34572ed6600524fa6f6b3a973da117
Zakjanzi <zakjanzi97@gmail.com>
2022-07-08 01:16:05 +0300

    middleware added v2

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express, { application } from "express"
+
 const app = express()
+
+//middleware
+notFoundMiddleware
+import notFoundMiddleware from "./middleware/not-found.js"
 
 app.get("/", (req, res) => {
     res.send("welcome")
 })
+
+app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5000
 
